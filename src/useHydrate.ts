bdf87f21a9807b80ee50cfbf87bb5c9a8dc29706
Mikaël Sévigny <sevignator@hotmail.com>
2024-01-27 16:55:00 -0500

    Add @author block tag

--- src/useHydrate.ts
+++ src/useHydrate.ts
@@ -1,8 +1,8 @@
 /**
- * Hook taken from Kent C. Dodds' Professional Web Forms workshop.
- *
  * @description Allows you to check whether the React code has be hydrated
  * prior to running some code.
+ *
+ * @author Kent C. Dodds (Professional Web Forms workshop)
  */
 
 import React from 'react';
